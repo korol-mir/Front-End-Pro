4460518f77d2b99cb4e7d46fef5fa2a9fc204fff
Miran <korolmiran772@gmail.com>
2026-01-29 12:15:55 +0200

    did home work ls3-1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log(3);
+let x = 3;
+console.log(x); //Number
+let big = 123456789123456789123456789n;
+console.log(big); //BigInt
+let name = "Miran";
+console.log(name); //String
+let cat = true;
+console.log(cat); //Boolean
+let un;
+console.log(un); //Underfined
+let nu = null;
+console.log(nu); //Null
+let id = Symbol("id");
+console.log(id); //Symbol
+let ob = [1, 2, 3];
+console.log(ob); //object
