edfb55c3125653cf9328b927d1514ee1d8b5e53e
Miran <korolmiran772@gmail.com>
2026-02-08 18:51:24 +0200

    did ls6-hw1

---
+++ script.js
@@ -0,0 +1,8 @@
+const infoForUser = {
+    name: prompt('Введіть ім\'я'),
+    age: prompt('Введіть ваш вік'),
+    city: prompt('Введіть ваше місто'),
+    pet: prompt('Ваша домашня тваринна'),
+};
+
+console.log(infoForUser);
