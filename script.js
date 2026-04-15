c1295790f3bba3919a8ab7157d7e2c11893fa064
Miran <korolmiran772@gmail.com>
2026-04-15 21:40:45 +0300

    did ls12-hw3

---
+++ script.js
@@ -0,0 +1,20 @@
+const taskList = document.querySelector('.task-list');
+const taskInput = document.querySelector('.task-input');
+const addTask = document.querySelector('.add-task');
+
+addTask.addEventListener('click', () => {
+    const task = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Видалити';
+    deleteButton.classList.add('btn-delete');
+    task.textContent = taskInput.value;
+    task.append(deleteButton);
+    task.classList.add('task');
+    taskList.append(task);
+});
+
+taskList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        event.target.parentElement.classList.add('delete');
+    }
+});
